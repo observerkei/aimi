56e4c72554801a41bf1bcf04d896ece87c031d77
observerkei <observerkei@foxmail.com>
2023-06-13 15:27:13 +0800

    feat: add task function wolfram api

--- core/task.py
+++ core/task.py
@@ -3,6 +3,7 @@ from typing import Dict, Any, List, Generator
 from pydantic import BaseModel, constr
 
 from tool.openai_api import openai_api
+from tool.wolfram_api import wolfram_api
 from tool.util import log_dbg, log_err, log_info, make_context_messages
 
 class TaskStepItem(BaseModel):
@@ -114,6 +115,9 @@ class Task():
                         log_dbg(f"set task: {str(task_info)} : {str(task_step)}")
                     elif task.call == 'critic':
                         self.critic(task.response)
+                    elif task.call == 'get_wolfram_response':
+                        response = self.get_wolfram_response(task.input)
+                        task.response = response
                     else:
                         log_err(f'no suuport call: {str(self.call)}')
                         continue
@@ -128,6 +132,20 @@ class Task():
             log_err(f"fail to load task res: {str(e)} : \n{str(res)}")
         
         yield ''
+    
+    def get_wolfram_response(
+        self,
+        input: str
+    ) -> str:
+        if not input or not len(input):
+            return 'input error'
+        answer = ''
+        for res in wolfram_api.ask(input):
+            if res['code'] != 0:
+                continue
+            answer = res['message']
+        
+        return answer
 
     def chat(
         self,
@@ -277,6 +295,13 @@ class Task():
                         }
                     ]
                 }
+            },
+            {
+                "call": "get_wolfram_response",
+                "execute": "system",
+                "description": "通过wolfram进行计算, 只有解数学题且能够转换为wolfram运算才能调用这个函数, 需要提前准备好wolfram能识别的输入数据.这个API有限制,请谨慎调用.得到真实响应后,需要通过chat接口给Master回复结果.",
+                "input": "在这里输入wolfram支持的内容.输入必须是英文.如: solve int x^2 dx",
+                "response": "wolfram的运行结果,调用的时候填None就行,调用成功后才有值,会返回是html或者txt文本"
             }
         ]
         self.tasks = {}
@@ -346,9 +371,15 @@ class Task():
 
         task = self.make_task(preset)
         setting = self.make_setting(aimi_name, task)
-        
-        chat = self.make_chat(question)
-        self.__append_running([chat])
+
+        # 如果只是想让任务继续, 就回复全空格/\t/\n
+        if (
+            len(question)
+            and not question.isspace()
+        ):
+            chat = self.make_chat(question)
+            self.__append_running([chat])
+
         running_format = self.get_running()
 
         context_messages = make_context_messages(running_format, setting, [])
@@ -391,11 +422,11 @@ class Task():
             f"你需要生成 {aimi_name} 的行为.",
             f"{aimi_name} 需要想办法完成 task_info 和 Master的要求. task_step 是完成步骤. 如果 task_step 为空, 或不符合,请重新设置步骤.",
             f"preset 是 {aimi_name} 的行为定义,只能对sync_tool的调用生效.",
-            f"每次新请求你只能再回复一次 execute 内容为 system 的方法. 尽量只通过单次调用完成回复.",
+            f"每次新请求你最多只能再调用一次 execute 内容为 system 的方法. 尽量只通过单次调用完成回复.",
             f"你需要思考如何接下我发送的内容,并且从中剔除我发送的部分,只留下你生成的,然后基于现有的timestamp填个新的再发给我.",
             f"如果我说停止当前计划,你还是需要保持调用 sync_tool 方法, 但是需要把当前 task_info 清空.",
             f"响应要求:请控制你的回复长度在3500字内.",
-            f"请保持你的回复可以被Python的`json.loads`解析,json外部不需要反引号包裹.json的参数内部不要有双引号,请严格按照以下格式回复我.(请用 [{{}},{{}}] 方式回复我):{response_format}"
+            f"请保持你的回复可以被Python的`json.loads`解析,json外部不需要反引号包裹.json的参数内部不要有双引号,请严格按照以下格式回复我.(请用 [{{task}}, {{task}}, ...] 方式回复我):{response_format}"
         ]
         setting = '\n'.join(setting)
 
