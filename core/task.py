90bdf7cd30b10bacdefc99df3d3004a5f447b765
observerkei <observerkei@foxmail.com>
2023-06-15 11:06:22 +0800

    style: set task api trans

--- core/task.py
+++ core/task.py
@@ -278,14 +278,14 @@ class Task():
             ),
             SyncToolItem(
                 call="set_task_step",
-                description="如果 task_step 和目标(task_info)不符合或为空, 则需要设置一下",
+                description="如果 task_step 和目标(task_info)不符合或者和Master要求不符合或为空, 则需要设置一下, 如果某步骤执行完毕, 也需要设置一下新步骤.",
                 input={
                     "task_id": "需要设置的task 对应的 id",
                     "task_step": [
                         {
                             "id": "序号, 为数字, 如: 1",
-                            "step": "在这里填写能够完成计划task_info的步骤",
-                            "check": "达成什么条件才算完成",
+                            "step": "在这里填写能够完成计划 task_info 的步骤",
+                            "check": "达成什么条件才算完成步骤",
                             "call": "应该调用什么 action 处理步骤."
                         }
                     ]
@@ -318,13 +318,13 @@ class Task():
             SyncToolItem(
                 call="get_wolfram_response",
                 description="通过wolfram进行计算, 所有计算都可以通过这个函数解决, 这个函数调用成功后是完全正确的.",
-                input="在这里输入wolfram支持的内容, 只支持英文内容. 如: solve int x^2 dx ",
+                input="在这里输入wolfram支持的内容, 翻译成英文再调用. 如: solve int x^2 dx ",
                 execute="system"
             ),
             SyncToolItem(
                 call="get_bard_response",
                 description="通过互联网进行搜索, 需要了解任何有时效性的内容都可以调用, 只能搜索最新有时效性的信息, 比如时间/日期或者某个网址的内容等.",
-                input="在这里输入要bard进行检索的内容, 输入必须为英文. 如: What time is it now?",
+                input="在这里输入要bard进行检索的内容, 翻译成英文再调用. 如: What time is it now?",
                 execute="system"
             )
         ]
@@ -448,11 +448,11 @@ class Task():
             "settings": [
                 f"action_tool 里面定义了所有你能调用的 方法(action).",
                 f"你每次生成追加内容时, 可以同时生成多个方法(action), 可以生成不限次 action->execute 为 AI 的方法, 可以进行很多思考.",
-                f"无论历史是什么, 你最多只能生成一次 action->execute 为 system 的方法. 每次都尽量生成一次 system 方法.",
+                f"无论历史是什么, 你最多只能生成一次 action->execute 为 system 的方法. 每次都尽量生成一次 system 方法. ",
                 f"当你在调用 action_tool 中 execute 是 system 的 action 时不要填写 response, 也不要说明任何和 response 有关内容, 除非调用成功.",
                 f"task 中定义了 {aimi_name} 你当前任务, 其中 task_info 是任务目标, task_step 是完成 task_info 需要进行的步骤, 步骤要和 action强绑定.",
                 f"如果 task_step 为空, 或不符合, 请重新设置步骤, 如果没有进展, 尽量给出创造性建议或优化步骤推进任务进度.",
-                f"在Master补充达成条件后, 你应该在保障任务能完成基础上, 修改任务步骤."
+                f"如果Master提出了要求, 你要修改当前步骤来满足要求.",
                 f"每次任务/关健操作完成, 都应该试探性地带上分析上报Master.",
                 f"你将扮演 {aimi_name}. 你会遵守 settings, 你通过 action_tool 行动. 你叫我 Master.",
                 f"preset 是 {aimi_name} 的预设, preset 只能对 action_tool 中定义的方法的输入生效.",
