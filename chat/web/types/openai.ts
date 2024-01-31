d3cec9b90bfd25680dee62a5e89400cf1ad296e9
observerkei <observerkei@foxmail.com>
2024-01-31 13:32:48 +0800

    fix: add OpenAIModels owned_by

--- chat/web/types/openai.ts
+++ chat/web/types/openai.ts
@@ -24,23 +24,27 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
+    owned_by: 'openai',
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
     name: 'GPT-3.5',
     maxLength: 12000,
     tokenLimit: 4000,
+    owned_by: 'openai',
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
+    owned_by: 'openai',
   },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
     name: 'GPT-4-32K',
     maxLength: 96000,
     tokenLimit: 32000,
+    owned_by: 'openai',
   },
 };
