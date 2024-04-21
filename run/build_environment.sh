5f0270e2ee4f1713f42159c73d4ed7801e2e5c43
observerkei <observerkei@foxmail.com>
2024-04-21 13:33:23 +0800

    refactor: add firefox open url

--- run/build_environment.sh
+++ run/build_environment.sh
@@ -4,7 +4,12 @@ function setup_termux() {
     pkg install -y tur-repo x11-repo
     pkg update
     pkg install -y rust python-grpcio binutils 
+
+    # install aimi_plugin/action/chat_to_open_url of firefox
+    pkg install firefox -y
+    pkg install geckodriver -y
 }
+
 
 if [[ -n "$TERMUX_VERSION" ]]; then
     setup_termux
