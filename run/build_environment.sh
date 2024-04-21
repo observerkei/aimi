934e15f0768cdb548c1fa14f9cbfa4b6dd944842
observerkei <observerkei@foxmail.com>
2024-04-21 14:19:22 +0800

    style: up dockerfile

--- run/build_environment.sh
+++ run/build_environment.sh
@@ -3,7 +3,9 @@
 function setup_termux() {
     pkg install -y tur-repo x11-repo
     pkg update
-    pkg install -y rust python-grpcio binutils 
+    pkg install -y rust python-grpcio binutils \
+        libandroid-spawn ninja python-pandas \
+        patchelf make cmake
 
     # install aimi_plugin/action/chat_to_open_url of firefox
     pkg install firefox -y
@@ -15,11 +17,7 @@ if [[ -n "$TERMUX_VERSION" ]]; then
     setup_termux
 fi
 
-
-apt-get install -y libandroid-spawn make cmake \
-    python3 python3-distutils python3-pip \
-     patchelf ninja python-pandas \
-
+apt-get install -y python3 python3-distutils python3-pip
 
 pip3 install -r ./requirements.txt 
 
